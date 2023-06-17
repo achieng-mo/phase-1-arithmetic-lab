e3fd2fc87a854f4877fbff8534cb388d562d6f65
Morine Achieng <morineachieng@Morines-MacBook-Air.local>
2023-06-17 07:55:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+//Solution 1:Create variable multiply to an equation that will multiply variables num1 and num2; 
+//...the result of the multiplication should be 62.
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+//Solution 2: Create a variable called random that will generate a random integer greater than 0
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+//Solution 3: Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; 
+//...the remainder should be 4.
+
+let num3 = 6
+let num4 = 16
+let mod = num4 % num3;
+console.log(mod);
+
+//Solution 4: Create a variable called max that finds the highest number in a set; the value returned should be 20
+
+const max = Math.max(3,14, 15, 20, 6, 18, 9);
+console.log(max);
